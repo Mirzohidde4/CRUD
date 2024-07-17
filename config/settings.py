f5775803c7a8e7e0b81d4037b9913b726e7bff62
Mirzohidde4 <mirzohidxudayberganov27@gmail.com>
2024-07-17 18:21:19 +0500

    static

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-6($j&_6xy4u^_-q-ho_av!ml%ari7-@xqcytv9^0lt((y!wo*s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
