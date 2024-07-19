1d24991309c4a116765057311131c8c02a9bf2b8
Mirzohidde4 <mirzohidxudayberganov27@gmail.com>
2024-07-19 18:04:18 +0500

    acc settings

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SignUp
+from .views import SignUp, Delete, Edit
 
 
 urlpatterns = [
-    path('signup/', SignUp.as_view(), name='signup')
+    path('signup/', SignUp.as_view(), name='signup'),
+    path('delete/<int:pk>/', Delete.as_view(), name='deleteaccount'),
+    path('settings/<int:pk>/', Edit.as_view(), name='settings'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,23 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.models import User
 
 # Create your views here.
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class Edit(generic.UpdateView):
+    model = User
+    template_name = 'registration/settings.html'
+    fields = ['first_name', 'last_name']  
+    success_url = reverse_lazy('home')  
+
+
+class Delete(generic.DeleteView):
+    model = User
+    template_name = 'registration/deleteaccount.html'
+    success_url = reverse_lazy('home')
